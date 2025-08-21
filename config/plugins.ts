fb18e655e7aa94c624bbff6be4178454cb22e4ef
samer <judosamer555@gmail.com>
2025-08-21 21:18:00 +0300

    update plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
-      jwtSecret: env("JWT_SECRET") || env("ADMIN_JWT_SECRET"),
+      jwtSecret: "wnNxQvs8KIvIJcZBCGY6LmWuvgJdg4LSd4NGgmchCOlVH0+QllCbwmNiEaimaipALrkk4lDbhiTCoIORbl9iTA==",
     },
   },
 });
