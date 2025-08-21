98147ca0240f63e517a8e1e8f7410dd43a094194
samer <judosamer555@gmail.com>
2025-08-21 20:45:59 +0300

    add keys

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ["932910814c6d06bdb75780bfba9138b4", "40573a1188cd02d3aa82902a3c7c5ae8"],
   },
 });
