1f96d7ea6d469a609402ac5cf655fb22ba6d48b4
samer <judosamer555@gmail.com>
2025-08-21 21:02:17 +0300

    add jwt secret

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: ["932910814c6d06bdb75780bfba9138b4", "40573a1188cd02d3aa82902a3c7c5ae8"],
+    keys: env.array("APP_KEYS", ["932910814c6d06bdb75780bfba9138b4", "40573a1188cd02d3aa82902a3c7c5ae8"]),
   },
 });
